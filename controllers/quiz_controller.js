6c290c01ec86130ececed351ab548c15cb7cfdf4
Luis Muñoz Martin de la Sierra <luismmsoto@gmail.com>
2016-05-29 14:11:09 +0200

    Tema 8 y 9: BBDD y Postrges en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,32 @@
+var models = require('../models');
+
 // GET /question
 exports.question = function(req, res, next) {
-	var answer = req.query.answer || '';
-	res.render('quizzes/question', {question: 'Capital de Italia', answer: answer});
+	models.Quiz.findOne().then(function(quiz) {
+		if(quiz) {
+			var answer = req.query.answer || '';
+			res.render('quizzes/question', {question: quiz.question, answer: answer});
+		}
+		else {
+			throw new Error('No hay preguntas en la BDD.');
+		}
+	}).catch(function(error) {
+		next(error);
+	});
 };
 
 // GET /check
 exports.check = function(req, res, next) {
-
-	var answer = req.query.answer || '';
- 	var result = answer === 'Roma' ? 'Correcta' : 'Incorrecta';
- 	res.render('quizzes/result', { result: result, answer: answer });
+	models.Quiz.findOne().then(function(quiz) {
+		if(quiz) {
+			var answer = req.query.answer || '';
+ 			var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+ 			res.render('quizzes/result', { result: result, answer: answer });
+		}
+		else {
+			throw new Error('No hay preguntas en la BDD.');
+		}
+	}).catch(function(error) {
+		next(error);
+	});
 };
---
+++ models/index.js
@@ -0,0 +1,52 @@
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Postgres DATABASE_URL = postgres://axymlwwcznwmiz:-bsCjNaU-m1CB1NUBOc3GKjtRK@ec2-23-21-193-140.compute-1.amazonaws.com:5432/dbku3om732u7gq
+// SQLite   DATABASE_URL = sqlite:///
+//          DATABASE_STORAGE = quiz.sqlite
+var url, storage;
+
+if(!process.env.DATABASE_URL) {
+  url = "sqlite:///";
+  storage = "quiz.sqlite";
+} else {
+  url = process.env.DATABASE_URL;
+  storage = process.env.DATABASE_STORAGE || "";
+}
+
+
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(url,
+                              { storage: storage,
+                                omitNull: true
+                              });
+
+// Importar la definicion de la tabla Quiz de quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync()
+    .then(function() {
+        // Ya se han creado las tablas necesarias.
+        return Quiz.count()
+                .then(function (c) {
+                    if (c === 0) {   // la tabla se inicializa solo si está vacía
+                        return Quiz.create({ question: 'Capital de Italia',
+          	                                 answer: 'Roma'
+          	                               })
+                                   .then(function() {
+                                        console.log('Base de datos inicializada con datos');
+                                    });
+                    }
+                });
+    })
+    .catch(function(error) {
+        console.log("Error Sincronizando las tablas de la BBDD:", error);
+        process.exit(1);
+    });
+
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
