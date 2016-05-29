483dd6242212aa47b0cf24261781b0718cc4c74f
Luis Muñoz Martin de la Sierra <luismmsoto@gmail.com>
2016-05-29 17:00:49 +0200

    Tema 10: Lista de preguntas

--- models/index.js
+++ models/index.js
@@ -34,12 +34,12 @@ sequelize.sync()
         return Quiz.count()
                 .then(function (c) {
                     if (c === 0) {   // la tabla se inicializa solo si está vacía
-                        return Quiz.create({ question: 'Capital de Italia',
-          	                                 answer: 'Roma'
-          	                               })
-                                   .then(function() {
-                                        console.log('Base de datos inicializada con datos');
-                                    });
+                        return Quiz.bulkCreate([
+                            {question: 'Capital de Italia', answer: 'Roma'},
+                            {question: 'Capital de Portugal', answer: 'Lisboa'}
+                          ]).then(function() {
+                              console.log('Base de datos inicializada con datos');
+                          });
                     }
                 });
     })
