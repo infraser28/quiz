9419d9601dbe1c00fabb13d9986d838b5ffe5648
Luis Muñoz Martin de la Sierra <luismmsoto@gmail.com>
2016-06-01 00:19:30 +0200

    Tema 21: Login

--- app.js
+++ app.js
@@ -32,8 +32,15 @@ app.use(session({ secret: "Quiz 2016",
 app.use(methodOverride('_method', {methods: ["POST", "GET"]}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
-app.use('/', routes);
 
+// Helper dinamico:
+app.use(function(req, res, next) {
+   // Hacer visible req.session en las vistas
+   res.locals.session = req.session;
+   next();
+});
+
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
