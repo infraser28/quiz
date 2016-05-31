4341a07d5ba46629f0e598fca81a7c3dbaa0fc27
Luis Muñoz Martin de la Sierra <luismmsoto@gmail.com>
2016-05-31 23:37:55 +0200

    Tema 20: Gestión de usuarios

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,24 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-// Autoload de rutas que usen :quizId
+// Autoload de parámetros
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('userId', userController.load);  // autoload :userId
+
+// Definición de rutas de cuenta
+router.get('/users',                    userController.index);   // listado usuarios
+router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
+router.get('/users/new',                userController.new);     // formulario sign un
+router.post('/users',                   userController.create);  // registrar usuario
+router.get('/users/:userId(\\d+)/edit', userController.edit);     // editar información de cuenta
+router.put('/users/:userId(\\d+)',      userController.update);   // actualizar información de cuenta
+router.delete('/users/:userId(\\d+)',   userController.destroy);  // borrar cuenta
 
 //Definición de rutas de /quizzes
 router.get('/quizzes:format?',					quizController.index);
