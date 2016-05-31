cf76bbab1d0c01c9e103c6bdc3d1f091e3e9a8c0
Luis Muñoz Martin de la Sierra <luismmsoto@gmail.com>
2016-05-31 16:30:51 +0200

    Tema 18: Crear comentario

--- models/index.js
+++ models/index.js
@@ -26,7 +26,13 @@ var sequelize = new Sequelize(url,
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+var Comment = sequelize.import(path.join(__dirname,'comment'));
 
-// sequelize.sync() crea e inicializa tabla de preguntas en DB
+// Relaciones entre modelos
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -18,6 +19,10 @@ router.post('/quizzes',							quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',		quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',			quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',			quizController.destroy);
+
 router.get('/author', 							quizController.author);
+
+router.get('/quizzes/:quizId(\\d+)/comments/new',  commentController.new);
+router.post('/quizzes/:quizId(\\d+)/comments',     commentController.create);
 
 module.exports = router;
